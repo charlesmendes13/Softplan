70a80a2a334973c4ad9c4bbf556ae0e23f83e24b
charlesmendes13 <charlesluizmendes@gmail.com>
2020-07-04 00:09:11 -0300

    Criando Api1 e Teste de Integração Api1

---
+++ Softplan/test/Api1/Softplan.Api1.Tests/Scenarios/TaxaJurosTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace Softplan.Api1.Tests.Scenarios
+{
+    public class TaxaJurosTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public TaxaJurosTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async Task TaxaJuros_Get_ReturnsOkResponse(string metodo)
+        {
+            var client = _factory.CreateClient();
+
+            var request = new HttpRequestMessage(new HttpMethod(metodo), "/api/TaxaJuros");
+
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+        }
+    }
+}
