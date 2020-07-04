e577ba57da8ed751a0ca67c8079719b18b1cc531
charlesmendes13 <charlesluizmendes@gmail.com>
2020-07-04 00:42:02 -0300

    Criando Api2 e Teste de Integração Api2

---
+++ Softplan/test/Api2/Softplan.Api2.Tests/Scenarios/CalculaJurosTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Softplan.Api2.Tests.Scenarios
+{
+    public class CalculaJurosTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public CalculaJurosTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("GET", 100, 5)]
+        public async void CalculaJuros_Get_ReturnsOkResponse(string metodo, decimal valorInicial, int meses)
+        {
+            var client = _factory.CreateClient();
+
+            var request = new HttpRequestMessage(new HttpMethod(metodo), $"/api/calcularJuros?valorInicial={valorInicial}&meses={meses}");
+
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            result.Should().Be("105.1");
+        }
+    }
+}
