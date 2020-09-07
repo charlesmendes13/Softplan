f28ea949a9004cfcb72537aa8c4f0612839b7c95
charlesmendes13 <charlesluizmendes@gmail.com>
2020-09-07 01:23:32 -0300

    Criando Teste de Integraçâo Controller

---
+++ app/test/Api2/Softplan.Api2.Integration.Tests/Controller/CalculaJurosControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using Xunit;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Softplan.Api2.Domain;
+
+namespace Softplan.Api2.Integration.Tests.Controller
+{
+    public class CalculaJurosControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly Mock<ITaxaJurosService> _taxaJurosService = new Mock<ITaxaJurosService>();
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public CalculaJurosControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _taxaJurosService.Setup(x => x.ObterAsync()).Returns(Task.FromResult(0.01m));
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("GET", 100, 5)]
+        public async void CalculaJuros_Get_Returns_Ok_Response(string metodo, decimal valorInicial, int meses)
+        {
+            var client = _factory.WithWebHostBuilder(hostbuilder =>
+            {
+                hostbuilder.ConfigureTestServices((services) =>
+                {
+                    services.AddSingleton<ITaxaJurosService>(_taxaJurosService.Object);
+                });
+            })
+            .CreateClient();
+
+            var request = new HttpRequestMessage(new HttpMethod(metodo), $"/api/calculaJuros?valorInicial={valorInicial}&meses={meses}");
+
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            result.Should().Be("105.1");
+        }
+    }
+}
---
+++ app/test/Api2/Softplan.Api2.Integration.Tests/Controller/ShowMeTheCodeControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Softplan.Api2.Integration.Tests.Controller
+{
+    public class ShowMeTheCodeControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ShowMeTheCodeControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async void CalculaJuros_Get_Returns_Ok_Response(string metodo)
+        {
+            var client = _factory.CreateClient();
+
+            var request = new HttpRequestMessage(new HttpMethod(metodo), "/api/ShowMeTheCode");
+
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Should().NotBeNull();
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            result.Should().Be("https://github.com/charlesmendes13/softplan");
+        }
+    }
+}
